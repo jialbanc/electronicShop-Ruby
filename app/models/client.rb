196c68b8afe606a58abcb3dac0cbde9191339297
Jimmy Banchon <jialbanc@espol.edu.ec>
2014-02-17 17:32:06 -0500

    Agregando validaciones de los atributos del modelo client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,9 @@
 class Client < ActiveRecord::Base
 	has_many :carts, dependent: :destroy
+	validates :user, :presence => true, :uniqueness => true
+	
+	validates :passw, :presence => true, :confirmation => true, :length => { :minimum => 8 } , :message => "Ingrese mínimo 8 caracteres"
+	validates :passw_confirmation, :presence => true
+	
+	validates :nombre, :allow_blank => true
 end
