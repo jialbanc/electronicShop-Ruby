25b36cbdba91dfe5675d1db94a757ea9482b896a
Jimmy Banchon <jialbanc@espol.edu.ec>
2014-02-17 18:21:58 -0500

    Corregidas validaciones del modelo client y añadidas validaciones para modelo item y cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,7 @@
 class Cart < ActiveRecord::Base
 	belongs_to :client
 	has_and_belongs_to_many :items
+	
+	validates :idcart, :presence => true, :uniqueness => true
+	
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ActiveRecord::Base
 	has_and_belongs_to_many :carts
+	
+	validates :iditem, :presence => true, :uniqueness => true
+	
+	validates :valor, :numericality => true 
 end
